6b3f74078f40baf9f28c8988962ef588ef2bb2bd
pdefusco <pauldefusco@gmail.com>
2022-08-30 13:33:14 -0700

    updating dags

--- cde_jobs/bash_dag.py
+++ cde_jobs/bash_dag.py
@@ -14,8 +14,8 @@ default_args = {
         'start_date': pendulum.datetime(2020, 1, 1, tz="Europe/Amsterdam")
         }
 
-airflow_cdw_dag = DAG(
-        'dw_cf_dag',
+bash_airflow_dag = DAG(
+        'bash_dag',
         default_args=default_args,
         schedule_interval='@daily',
         catchup=False,
@@ -24,13 +24,13 @@ airflow_cdw_dag = DAG(
 
 spark_step = CDEJobRunOperator(
         task_id='sql_job_new',
-        dag=airflow_cdw_dag,
+        dag=bash_airflow_dag,
         job_name='sql_job'
         )
 
 shell = BashOperator(
         task_id='bash',
-        dag=airflow_cdw_dag,
+        dag=bash_airflow_dag,
         bash_command='echo "Hello Airflow" '
         )
 
@@ -40,7 +40,7 @@ show databases;
 
 dw_step3 = CDWOperator(
     task_id='dataset-etl-cdw',
-    dag=airflow_cdw_dag,
+    dag=bash_airflow_dag,
     cli_conn_id='cdw_connection',
     hql=cdw_query,
     schema='default',
@@ -48,4 +48,11 @@ dw_step3 = CDWOperator(
     query_isolation=True
 )
 
-spark_step >> shell >> dw_step3
+
+also_run_this = BashOperator(
+    task_id='also_run_this',
+    dag=bash_airflow_dag,
+    bash_command='echo "yesterday={{ yesterday_ds }} | today={{ ds }}| tomorrow={{ tomorrow_ds }}"',
+)
+
+spark_step >> shell >> dw_step3 >> also_run_this
